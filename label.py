e66de3f3ba80f2e1fc3471f2faf96ab92f612c7e
Josh31416 <33935790+josh31416@users.noreply.github.com>
2020-04-05 11:31:51 +0200

    refactor: Global variables for directories in label.py

--- label.py
+++ label.py
@@ -5,22 +5,36 @@ import sys
 import shutil
 from pathlib import Path
 
+class1 = "makeup"
+class2 = "no-makeup"
+new_dataset_folder = "celeba"
+dataset_folder = "img_align_celeba"
+
 def press(event):
     sys.stdout.flush()
-    Path("celeba/makeup/makeup").mkdir(parents=True, exist_ok=True)
-    Path("celeba/no-makeup/no-makeup").mkdir(parents=True, exist_ok=True)
+    class1_path = os.path.join(new_dataset_folder, class1, class1)
+    class2_path = os.path.join(new_dataset_folder, class2, class2)
+    
+    # create directories if they don't exist
+    Path(class1_path).mkdir(parents=True, exist_ok=True)
+    Path(class2_path).mkdir(parents=True, exist_ok=True)
+    
+    # assign to class with keyboard keys
+    # left arrow  -> class 1
+    # right arrow -> class 2
+    # x           -> delete image
     if event.key == 'left':
-        shutil.move(imagePath, "celeba/makeup/makeup/"+str(imagePath.split('/')[1]))
+        shutil.move(imagePath, class1_path+str(imagePath.split('/')[-1]))
         plt.close()
     elif event.key == 'right':
-        shutil.move(imagePath, "celeba/no-makeup/no-makeup/"+str(imagePath.split('/')[1]))
+        shutil.move(imagePath, class2_path+str(imagePath.split('/')[-1]))
         plt.close()
     elif event.key == 'x':
         Path(imagePath).unlink()
         plt.close()
 
         
-images = glob.glob('img_align_celeba/*.jpg')
+images = glob.glob(f"{dataset_folder}/*.jpg")
 for idx, imagePath in enumerate(images):
     print(f'{idx}/{len(images)} {imagePath}')
     image = mpimg.imread(imagePath)
